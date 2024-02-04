869e100f1fece86c7f862e50373396c2da22722b
Timothy May <tmay@inscico.com>
2024-02-04 07:19:29 -0800

    Added common package handling

---
+++ LazyStack.Notifications.DynamoDBRepo/GlobalUsing.cs
@@ -0,0 +1,15 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using LazyStack.DynamoDBRepo;
+global using LazyStack.Notifications.Repo;
+
+
+global using Amazon.DynamoDBv2;
+global using Amazon.DynamoDBv2.Model;
+global using Amazon.Runtime;
+global using LazyStack.ControllerBase;
+global using Microsoft.AspNetCore.Mvc;
+global using Newtonsoft.Json;
